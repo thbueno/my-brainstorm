482ff593e014a48583d8da1fc1c8765c561522cf
thbueno <thiagobueno.s@gmail.com>
2025-03-31 23:46:00 +0700

    implement sticky header with navigation links

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,22 @@
 "use client";
 
+import Link from "next/link";
+
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="sticky top-0 z-50 left-0 right-0">
+      <div>
+        {/* Left side */}
+        <div>
+          <Link href="/">
+            <h1>My brainstorm</h1>
+          </Link>
+        </div>
+        {/* Right side */}
+        <div></div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
