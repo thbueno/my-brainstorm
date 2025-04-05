c5e8c9b9fa14f7d06671c6f7727badccd11889a0
thbueno <thiagobueno.s@gmail.com>
2025-04-05 23:49:33 +0700

    uncomment AnalyseButton component in YoutubeVideoForm for video analysis submission

--- components/YoutubeVideoForm.tsx
+++ components/YoutubeVideoForm.tsx
@@ -1,6 +1,7 @@
 import Form from "next/form";
 // import AnalyseButton from "./AnalyseButton";
 import { analyseYoutubeVideo } from "@/actions/analyseYoutubeVideo";
+import AnalyseButton from "./AnalyseButton";
 
 function YoutubeVideoForm() {
   return (
@@ -16,7 +17,7 @@ function YoutubeVideoForm() {
           className="flex-1 w-full px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all duration-200"
         />
 
-        {/* <AnalyseButton /> */}
+        <AnalyseButton />
       </Form>
     </div>
   );
