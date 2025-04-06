33bcacde654a197ef8b31a1bd5c5d57c4c567512
thbueno <thiagobueno.s@gmail.com>
2025-04-07 00:00:00 +0700

    enter

--- components/YoutubeVideoForm.tsx
+++ components/YoutubeVideoForm.tsx
@@ -13,7 +13,7 @@ function YoutubeVideoForm() {
         <input
           name="url"
           type="text"
-          placeholder="add YouTube URL"
+          placeholder="Enter YouTube URL"
           className="flex-1 w-full px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all duration-200"
         />
 
