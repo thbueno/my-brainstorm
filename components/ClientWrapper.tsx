09fb8bcfd64f2ceb1c240a9a865d7c3cad5d51a7
thbueno <thiagobueno.s@gmail.com>
2025-04-07 23:32:49 +0700

    Wrap children in SchematicProvider within ClerkProvider in ClientWrapper component

--- components/ClientWrapper.tsx
+++ components/ClientWrapper.tsx
@@ -1,10 +1,15 @@
 "use client";
 import { ClerkProvider } from "@clerk/nextjs";
+import { SchematicProvider } from "@schematichq/schematic-react";
 
 export default function ClientWrapper({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <ClerkProvider>{children}</ClerkProvider>;
+  return (
+    <ClerkProvider>
+      <SchematicProvider>{children}</SchematicProvider>
+    </ClerkProvider>
+  );
 }
