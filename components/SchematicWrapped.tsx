d7d8f044026d14e0f367f4513b504362062ece8c
thbueno <thiagobueno.s@gmail.com>
2025-04-09 00:03:30 +0700

    Add useEffect hook to SchematicWrapped component for user and event handling

--- components/SchematicWrapped.tsx
+++ components/SchematicWrapped.tsx
@@ -2,10 +2,13 @@
 
 import { useUser } from "@clerk/nextjs";
 import { useSchematicEvents } from "@schematichq/schematic-react";
+import { useEffect } from "react";
 
 const SchematicWrapped = ({ children }: { children: React.ReactNode }) => {
   const { identify } = useSchematicEvents();
   const { user } = useUser();
+
+  useEffect(() => {}, [user, identify]);
 
   return <>{children}</>;
 };
