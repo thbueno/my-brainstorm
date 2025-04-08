b43af922c715b1fa56802b50a53f920eb143c272
thbueno <thiagobueno.s@gmail.com>
2025-04-09 00:11:28 +0700

    Implement user identification in useEffect hook of SchematicWrapped component

--- components/SchematicWrapped.tsx
+++ components/SchematicWrapped.tsx
@@ -8,7 +8,31 @@ const SchematicWrapped = ({ children }: { children: React.ReactNode }) => {
   const { identify } = useSchematicEvents();
   const { user } = useUser();
 
-  useEffect(() => {}, [user, identify]);
+  useEffect(() => {
+    const userName =
+      user?.username ??
+      user?.fullName ??
+      user?.emailAddresses[0]?.emailAddress ??
+      user?.id;
+
+    if (user?.id) {
+      identify({
+        // Company Level key
+        company: {
+          keys: {
+            id: user.id,
+          },
+          name: userName,
+        },
+
+        // User Level key
+        keys: {
+          id: user.id,
+        },
+        name: userName,
+      });
+    }
+  }, [user, identify]);
 
   return <>{children}</>;
 };
