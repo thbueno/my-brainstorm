5e4c7d145f8aaf7ec2de1ced39468ab848d743e8
thbueno <thiagobueno.s@gmail.com>
2025-04-09 00:01:49 +0700

    Add imports and hooks to SchematicWrapped component for user and event handling

--- components/SchematicWrapped.tsx
+++ components/SchematicWrapped.tsx
@@ -1,6 +1,12 @@
 "use client";
 
+import { useUser } from "@clerk/nextjs";
+import { useSchematicEvents } from "@schematichq/schematic-react";
+
 const SchematicWrapped = ({ children }: { children: React.ReactNode }) => {
+  const { identify } = useSchematicEvents();
+  const { user } = useUser();
+
   return <>{children}</>;
 };
 
