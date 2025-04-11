599ccd36c81d5a78c9f47c4bd09e6660117e1e23
thbueno <thiagobueno.s@gmail.com>
2025-04-11 23:26:30 +0700

    Implement ManagePlan component with layout and SchematicComponent integration

--- app/manage-plan/page.tsx
+++ app/manage-plan/page.tsx
@@ -1,8 +1,16 @@
+import SchematicComponent from "@/components/schematic/SchematicComponent";
 
 function ManagePlan() {
   return (
-    <div>page</div>
-  )
+    <div className="container mx-auto p-4 md:p-0">
+      <h1 className="text-2xl font-bold mb-4 my-8">Manage Your Plan</h1>
+      <p className="text-gray-600 mb-8">
+        Manage your subscription and billing details here.
+      </p>
+
+      <SchematicComponent componentId="cmpn_bYHaGFLE2Xg" />
+    </div>
+  );
 }
 
-export default ManagePlan
+export default ManagePlan;
