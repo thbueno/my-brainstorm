53373a0370268e6023d7c95b6c3416d7fdf0de7c
thbueno <thiagobueno.s@gmail.com>
2025-04-13 21:52:26 -0700

    Refactor AnalysisPage component layout for improved clarity and organization

--- app/video/[videoId]/analysis/page.tsx
+++ app/video/[videoId]/analysis/page.tsx
@@ -1,11 +1,18 @@
 function AnalysisPage() {
   return (
     <div className="xl:container mx-auto px-4 md:p-0">
-      <div className="grid  grid-cols-1 lg:grid-cols-2 gap-2">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
         {/* Left side */}
-        <div>Analysis Left</div>
+        <div className="">
+          {/* analysis section */}
+          {/* youtube video details */}
+          {/* THumbnail generation */}
+          {/* title generation */}
+          {/* transcription */}
+          <p>Analysis Left</p>
+        </div>
         {/* Right side */}
-        <div>Analysis right</div>{" "}
+        <div>Analysis right</div>
       </div>
     </div>
   );
