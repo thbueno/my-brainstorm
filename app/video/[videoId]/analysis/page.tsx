b779d048930145a8c952c1ec214065090db5e87d
thbueno <thiagobueno.s@gmail.com>
2025-04-13 21:42:54 -0700

    Refactor AnalysisPage component to separate left and right sections

--- app/video/[videoId]/analysis/page.tsx
+++ app/video/[videoId]/analysis/page.tsx
@@ -2,8 +2,10 @@ function AnalysisPage() {
   return (
     <div className="xl:container mx-auto px-4 md:p-0">
       <div className="grid  grid-cols-1 lg:grid-cols-2 gap-2">
-        {" "}
-        Analysis Page 3
+        {/* Left side */}
+        <div>Analysis Left</div>
+        {/* Right side */}
+        <div>Analysis right</div>{" "}
       </div>
     </div>
   );
