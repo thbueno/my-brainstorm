eb4507bfd5e7532195cb5bf41e9ea5c8f673f53b
thbueno <thiagobueno.s@gmail.com>
2025-04-13 21:41:28 -0700

    Refactor AnalysisPage component layout for improved structure

--- app/video/[videoId]/analysis/page.tsx
+++ app/video/[videoId]/analysis/page.tsx
@@ -1,5 +1,12 @@
 function AnalysisPage() {
-  return <div>Analysis Page 3 </div>;
+  return (
+    <div className="xl:container mx-auto px-4 md:p-0">
+      <div className="grid  grid-cols-1 lg:grid-cols-2 gap-2">
+        {" "}
+        Analysis Page 3
+      </div>
+    </div>
+  );
 }
 
 export default AnalysisPage;
