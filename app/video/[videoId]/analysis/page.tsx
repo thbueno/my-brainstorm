5854f911a13f90839578ba2234789316d9511210
thbueno <thiagobueno.s@gmail.com>
2025-04-13 21:56:20 -0700

    AnalysisPage component layout for better section organization

--- app/video/[videoId]/analysis/page.tsx
+++ app/video/[videoId]/analysis/page.tsx
@@ -3,16 +3,19 @@ function AnalysisPage() {
     <div className="xl:container mx-auto px-4 md:p-0">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
         {/* Left side */}
-        <div className="">
+        <div className=" order-2 md:order-1 lg:order-1">
           {/* analysis section */}
           {/* youtube video details */}
           {/* THumbnail generation */}
           {/* title generation */}
           {/* transcription */}
-          <p>Analysis Left</p>
+          <p>Youtube video</p>
         </div>
         {/* Right side */}
-        <div>Analysis right</div>
+        <div className=" order-1 md:order-2 lg:order-2">
+          {/* AI agent Chat section */}
+          <p>chat</p>
+        </div>
       </div>
     </div>
   );
